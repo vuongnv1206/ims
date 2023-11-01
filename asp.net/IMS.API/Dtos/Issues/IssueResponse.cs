1d291cf86bf9a8c70aac40933bf0c7c85dcdc793
Tlinh <tuanlinhnguyen3092@gmail.com>
2023-11-01 23:14:03 +0700

    update dtos

---
+++ asp.net/IMS.API/Dtos/Issues/IssueResponse.cs
@@ -0,0 +1,15 @@
+﻿using IMS.Contract.Common.Paging;
+using IMS.Contract.Contents.Milestones;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.Api.Dtos.Issues
+{
+    public class IssueResponse : PagingResponsse
+    {
+        public List<IssueDto> Issues { get; set; }
+    }
+}
